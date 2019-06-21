21f9f410a60bf7576be1feb46c8e7bff1129ee35
Trevor Gollaher <trevorgollaher@Trevors-Air.fios-router.home>
2019-06-21 00:23:35 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,9 +1,26 @@
 //
-console.log("Up and running!");
 
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+alert('Hello, friends.'); 
 
-console.log ("User flipped " + cardOne);
+const cards = ["queen", "queen", "king", "king"];
+
+const cardsInPlay = [];
+
+const cardOne = cards[0];
+
+cardsInPlay.push("cardOne");
+console.log("User flipped the queen!");
+
+const cardTwo = cards[2];
+
+cardsInPlay.push("cardTwo");
+console.log("User flipped king!");
+
+if (length.cardsInPlay === 2) {
+	
+
+} else if (cardsInPlay[0] === cardsInPlay[1]) {
+  result = alert("You found a match!");
+} else if (cardsInPlay[0] !== cardsInPlay[1]) {
+  result = alert("Sorry, try again.");
+}
