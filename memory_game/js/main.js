7868e5e41c52426b7b9d736f3f06fdb79f22e875
Trevor Gollaher <trevorgollaher@Trevors-Air.fios-router.home>
2019-06-23 01:14:15 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,7 +2,29 @@
 
 alert('Hello, friends.'); 
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+    {
+        rank: "queen",
+        suit: "hearts",
+        cardImage: "images/queen-of-hearts.png",
+    },
+    {
+        rank: "queen",
+        suit: "diamonds",
+        cardImage: "images/queen-of-diamonds.png",
+    },
+    {
+        rank: "king",
+        suit: "hearts",
+        cardImage: "images/king-of-hearts.png",
+    },
+    {
+        rank: "king",
+        suit: "diamonds",
+        cardImage: "images/king-of-diamonds.png",
+    } 
+  
+  ];
 
 var cardsInPlay = []; 
 
@@ -15,17 +37,20 @@ function checkForMatch() {
 };
 
 function flipCard(cardId) {
-checkForMatch();
-/* if (length.cardsInPlay === 2) {
-	
+
+ if (length.cardsInPlay === 2) {
+	checkForMatch();
  } else if (cardsInPlay[0] === cardsInPlay[1]) {
   result = alert("You found a match!");
  } else if (cardsInPlay[0] !== cardsInPlay[1]) {
   result = alert("Sorry, try again.");
  } 
-*/
- console.log("User flipped " + cards[cardId]);
- cardsInPlay.push("cards[cardId]");
+
+ console.log("User flipped " + cards[cardId].rank);
+ cardsInPlay.push("cards[cardId].rank");
+ console.log(cards[cardId].cardImage);
+ console.log(cards[cardId].suit);
+
 };
 flipCard(0);
 flipCard(2);
